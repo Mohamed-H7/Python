329630789420f96f522ddd83588bd587b0362699
Mohamed Hamdo <mhmdmageedmhmd1990@gmail.com>
2023-02-28 01:59:24 +0300

    Count letters and numbers

---
+++ Count letters and numbers/letter_number.py
@@ -0,0 +1,50 @@
+# يستعمل هذا الكود لمعرفة عدد الاحرف او الارقام الموجودة داخل الملف
+# يوجد دالة ايضا لمعرفة كم سطر داخل الملف
+# يجب استدعاء الدالة ووضع اسم الملف ولكن بدون صيغة الملف فقط الاسم
+
+def num_of_characters(file_name):
+
+    letters_big = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+                   "S", "T", "Y", "V", "W", "X", "Y", "Z"]
+
+    letters_small = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                     "s", "t", "y", "v", "w", "x", "y", "z"]
+
+    file_r = open(f"{file_name}.txt", "r")
+    line = file_r.read()
+    count_letter = 0
+
+    for i in line:
+        if i in letters_small or i in letters_big:
+            count_letter += 1
+
+    print(count_letter)
+    file_r.close()
+
+
+def num_of_digits(file_name):
+    number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+    file_r = open(f"{file_name}.txt", "r")
+    line = file_r.read()
+    count_number = 0
+
+    for i in line:
+        if i in number:
+            count_number += 1
+
+    print(count_number)
+    file_r.close()
+
+
+def line(file_name):
+    file_r = open(f"{file_name}.txt", "r")
+    line = file_r.readlines()
+
+    print(len(line))
+    file_r.close()
+
+
+num_of_characters("Letter")
+num_of_digits("Letter")
+line("Letter")
